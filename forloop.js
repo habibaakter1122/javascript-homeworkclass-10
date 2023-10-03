fbef94bd0aeb121975b656ef9dbe3599f22a980c
Habiba Akter <mshabiba573@gmail.com>
2023-10-03 22:53:32 +0600

    all

--- forloop.js
+++ forloop.js
@@ -1,6 +1,51 @@
 
 for(let i = 0; i<10; i++){
-    console.log('for loop');
+    // console.log(i + 1);
 }
+
+
+
+for (let i = 1; i <=10;i++){
+    //  console.log(i * 10);
+}
+  
+for (let i = 1; i <=10;i++){
+    //  console.log(i,'*',10,'=',(i*10));
+}
+
+for (let i = 3; i <=12;i++){
+    // console.log(i,'x',12,'=',(i*12));
+}
+
+function namta (num) {
+    for (let i = 1; i <= 10; i++) {
+        // console.log(num, 'x', i, '=', (i * num));
+    }
+}
+namta(1)
+namta(2)
+namta(3)
+namta(4)
+namta(5)
+namta(6)
+namta(7)
+namta(8)
+ 
+for (let i = 1; i <= 10; i++){
+       for (let j = 1; j <= 10; j++){
+        // console.log(i, 'x', j,'=', (i * j));
+       }
+}
+
+
+for (let i = 1; i <=20; i++){
+
+      if (i < 11){
+        continue;
+      }
+    console.log(i);
+}
+
+
 
 
---
+++ object.js
@@ -0,0 +1,9 @@
+let student = { name :'alinoor', age:26, class:9,roll:15,}
+let mobileDevice = {name:'vivo',model:'y20',price:15000}
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.class);
+// console.log(student.roll);
+console.log(mobileDevice.name);
+console.log(mobileDevice.model);
+console.log(mobileDevice.price);
