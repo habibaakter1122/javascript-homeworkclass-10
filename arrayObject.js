e7f3eee188a398f57931a79643031e5c5fd550ed
Habiba Akter <mshabiba573@gmail.com>
2023-10-06 21:40:10 +0600

    all

---
+++ arrayObject.js
@@ -0,0 +1,56 @@
+// let student = {name:'asha', age:18, class:'dipolama'}
+
+
+// Array
+let name = 'mitu'
+name = 'anika'
+
+
+
+const fruit = 'mango'; 
+// console.log(name);
+// console.log(fruit);
+
+// Array 
+const student_name_array = [ 'mitu',  'israt',  'dola',  'alif']
+student_name_array.push ('kona')
+student_name_array.pop()
+student_name_array.unshift('hamle')
+student_name_array.shift()
+// console.log(student_name_array); 
+// console.log(student_name_array[1]);
+
+
+// for of
+for (const name of student_name_array ){
+    // console.log(name +  ''  +  'chowdury');
+} 
+
+ 
+const student = {name: 'samiya', age:12, class:5,} 
+const nameStorer = student['name']
+const ageStorer = student.age
+const classStorer = student.class
+// console.log(nameStorer); 
+// console.log(ageStorer);
+// console.log(classStorer);
+
+
+student . roll = 112543;
+// console.log(student);
+
+const Developer = { name: 'Tamim', Id:1920066, Salary:22000 }
+const keysOrPopertyName = Object.keys(Developer)
+const PopertyValues = Object.values(Developer)
+console.log('poperty Name =', keysOrPopertyName);
+// console.log(Developer);
+console.log('poperty Values=', PopertyValues);
+
+
+
+
+
+
+
+
+
---
+++ function.js
@@ -0,0 +1,77 @@
+// loop -> condition fullfilled -> repeating
+
+// let const
+// function
+
+ function displyName () {
+
+    // console.log("um printing");
+ }
+
+// displyName()
+
+// 
+function startMarket (){
+    // console.log('jamakapara');
+}
+// startMarket();
+
+
+// function with parameter
+function add (){
+    // console.log(2+6);
+}
+// add();
+
+function Biyoga(){
+    // console.log(9 - 3);
+}
+// Biyoga();
+//  
+function add (value1, value2){
+
+    // console.log(value1 + value2);
+} 
+// add(10,25);
+// add(17,26);
+
+// 
+function Biyoga (value1,value2){
+    // console.log(value1 - value2);
+} 
+Biyoga(45,12);
+// 
+function multy (value1){
+    // console.log(value1 * 10 );
+}
+
+// multy(9);
+// multy(10);
+// multy(15);
+
+function add (value1,value2){
+    const total = value1 + value2
+    // console.log(total);
+}
+const a = 10;
+const b = 19;
+add(a,b);
+
+// function with return
+function add (value1, value2) {
+    const total = value1 + value2 
+    return total;
+}
+ const  storedvalue = add(4, 3)
+    //  console.log(storedvalue);
+     const sumvalue = add (3,6)
+    //  console.log(sumvalue + 11);
+
+
+    //  short 
+    function add (value1,value2){
+       
+        return value1 + value2
+    }
+    const sumvalue1 = add (3,6)
+    console.log(sumvalue1 + 15);
